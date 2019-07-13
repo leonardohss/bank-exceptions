18ba2a2876fb31ac898341d87efd07ab561deb7c
Leonardo Sousa <leorick97@gmail.com>
2019-07-13 12:37:41 -0300

    Create the project

---
+++ BankExceptions/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankExceptions.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance =+ amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance =- amount;
+        }
+
+        public override string ToString()
+        {
+            return "Account: "
+                + Number
+                + ", Holder: "
+                + Holder
+                + ", Balance: "
+                + Balance.ToString("F2");
+        }
+    }
+}
---
+++ BankExceptions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BankExceptions.Entities;
+
+namespace BankExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1, "Peter", 300.00);
+
+            account.Deposit(400.00);
+
+            Console.WriteLine(account);
+        }
+    }
+}
